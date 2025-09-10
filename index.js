9039d6f1c96f9c4e74db4e87e8d21e513b884dfc
Luis Manuel Tapia Bautista <36052812+luismtapia@users.noreply.github.com>
2025-09-09 23:49:53 -0600

    Update index.js
    v 2.0.0

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const words = require('./analyzer-text');
+const analizador = require('./analyzer-text');
+
+// Texto
+const textoEjemplo = `Hola Luis,
+Este es un párrafo de prueba con números como 123 y símbolos como @, #, $.`;
+
+// Archivo .txt
+const fs = require('fs')
+const path = require('path')
+
+const filepath = path.join(__dirname, 'input.txt');
+const archivoEjemplo = fs.readFileSync(filepath, 'utf-8');
 
 
-console.log("espacios :", words.espacios());
-console.log("lineas :", words.lineas());
-console.log("palabras :", words.palabras());
-console.log("letras :", words.letras());
-console.log("numeros :", words.numeros());
-console.log("parrafos :", words.parrafos());
-console.log("caracteres especiales :", words.caracteresEspeciales());
-console.log("total caracter dado (o) : ", words.totalCaracter('o'));
+// Ejemplo texto
+console.log("espacios :", analizador.espacios(textoEjemplo));
+console.log("lineas :", analizador.lineas(textoEjemplo));
+console.log("palabras :", analizador.palabras(textoEjemplo));
+console.log("letras :", analizador.letras(textoEjemplo));
+console.log("numeros :", analizador.numeros(textoEjemplo));
+console.log("parrafos :", analizador.parrafos(textoEjemplo));
+console.log("caracteres especiales :", analizador.caracteresEspeciales(textoEjemplo));
+console.log("total caracter dado (o) : ", analizador.totalCaracter(textoEjemplo, 'o'));
+
+
+// Ejemplo archivo
+console.log("\nespacios :", analizador.espacios(archivoEjemplo));
+console.log("lineas :", analizador.lineas(archivoEjemplo));
+console.log("palabras :", analizador.palabras(archivoEjemplo));
+console.log("letras :", analizador.letras(archivoEjemplo));
+console.log("numeros :", analizador.numeros(archivoEjemplo));
+console.log("parrafos :", analizador.parrafos(archivoEjemplo));
+console.log("caracteres especiales :", analizador.caracteresEspeciales(archivoEjemplo));
+console.log("total caracter dado (i) : ", analizador.totalCaracter(archivoEjemplo,'i'));
